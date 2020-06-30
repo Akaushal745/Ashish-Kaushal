2af91d10a2944a65d602e3af17e03aac3857a669
Akaushal745 <67637740+Akaushal745@users.noreply.github.com>
2020-06-30 18:27:27 +0530

    week 1 assignment

---
+++ Capston Project.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## This notebook will be mainly used for the capstone project.
+
+# In[6]:
+
+
+import pandas as pd
+
+
+# In[7]:
+
+
+import numpy as np
+
+
+# In[8]:
+
+
+print(" Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
